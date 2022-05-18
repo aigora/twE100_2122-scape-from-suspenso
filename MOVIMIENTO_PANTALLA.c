22a8a6372961d311d49256ee22378c910c37aeab
edu.albelda <e.albelda@alumnos.upm.es>
2022-05-18 16:37:14 +0200

    Update MOVIMIENTO_PANTALLA.c

--- MOVIMIENTO_PANTALLA.c
+++ MOVIMIENTO_PANTALLA.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 //acceso a componentes de una estructura
 typedef struct {
-int x=0,y=0;
-} punto;
+int x,y;
+}punto;
 
+
+int pantalla(punto xy);
 // Función para controlar el desplazamiento horizontal, siendo xy el
 // punto, y n el número de unidades a desplazar
 void horizontal(punto *xy, int n);
@@ -12,57 +15,63 @@ void vertical(punto *xy, int n);
 
 int main () {
 punto xy;
+xy.x=19;
+xy.y=29;
 int n=0;
 char letra;
-printf("El programa funciona con la interacción del usuario a partir de las teclas w (arriba), a (izquierda), x (abajo), d (derecha), y sus correspondientes mayúsculas./n Por ejemplo, si el usuario aprieta la tecla w el punto se desplaza haciaarriba una unidad,/n y si aprieta W se desplaza cinco unidades hacia arriba.\n");
 
+//printf("El programa funciona con la interacción del usuario a partir de las teclas w (arriba), a (izquierda), x (abajo), d (derecha), y sus correspondientes mayúsculas./n Por ejemplo, si el usuario aprieta la tecla w el punto se desplaza haciaarriba una unidad,/n y si aprieta W se desplaza cinco unidades hacia arriba.\n");
+pantalla(xy);
 do{
-scanf("%c", &letra);
+scanf(" %c", &letra);
 switch(letra){
 	case 'a':
-	n=-1;
+	n=1;
 	horizontal( &xy, n);
 	break;
 
 	case 'A':
-	n=-1;
+	n=1;
 	horizontal( &xy, n);
 	break;
 
 	case 'd':
-	n=1;
+	n=-1;
 	horizontal( &xy, n);
 	break;
 
 	case 'D':
-	n=1;
+	n=-1;
 	horizontal( &xy, n);
 	break;
 
 	case 'w':
-	n=1;
+	n=-1;
 	vertical( &xy, n);
 	break;
 
 	case 'W':
-	n=1;
+	n=-1;
 	vertical( &xy, n);
 	break;
 
 	case 's':
-	n=-1;
+	n=1;
 	vertical( &xy, n);
 	break;
 
 	case 'S':
-	n=-1;
+	n=1;
 	vertical( &xy, n);
 	break;
 
 }
-
+system("cls");
 printf("El punto es (%d,%d)\n", xy.x, xy.y);
-}while(letra != 's');
+
+pantalla(xy);
+
+}while(letra != 'x');
 
 
 return 0;
@@ -82,7 +91,7 @@ void vertical(punto *xy, int n){
 
 }
 
-int pantalla(){
+int pantalla(punto xy){
 int fil, col, caso;
 char pared=254, columna=219, columna_arriba=220, columna_abajo=223;
 int pantalla[20][30]={
@@ -111,6 +120,10 @@ int pantalla[20][30]={
 
 	for(fil=0; fil<20; fil++){
 		for(col=0; col<30; col++){
+            if (pantalla[fil][col]==0 && fil==xy.x && col==xy.y){
+                  printf("O");
+            }
+
 		    if(pantalla[fil][col]==1){
                     printf("%c", pared);
 
