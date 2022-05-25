8b353df7e68b691023132ab3988efef316930776
edu.albelda <e.albelda@alumnos.upm.es>
2022-05-26 01:32:52 +0200

    He empezado con la mezcla de programas
    He menu dificultad y MOVIMIENTO_PANTALLA en SCAPEROOM

--- SCAPEROOM.c
+++ SCAPEROOM.c
@@ -5,6 +5,7 @@ typedef struct {
 int x,y;
 }punto;
 
+int movimiento_pantalla(int dificultad);
 
 int pantalla(punto xy, int dificultad);
 // Función para controlar el desplazamiento horizontal, siendo xy el
@@ -13,7 +14,74 @@ void horizontal(punto *xy, punto *punto_siguiente, int n);
 // Función para controlar el desplazamiento vertical
 void vertical(punto *xy, punto *punto_siguiente, int n);
 
-int main () {
+
+int main()
+{
+
+    int dificultad, puntuacion=0;
+
+
+
+
+    printf("%c%cBIENVENIDO AL SCAPE ROOM SCAPE FROM SUSPENSO!!\n", 173,173);
+    printf("En este scape room deberas de poner a prueba tus habilidades para superar todas los retos y acertijos que te encontraras\n");
+    printf("Pero antes, deberas de elegir la dificultad a la que deseas jugar y registrarte como usuario.\n");
+
+
+
+   do {
+     printf("Elige la dificultad a la que quieres jugar\n");
+     printf("(1)FACIL\n");
+     printf("(2)MEDIO\n");
+     printf("(3)FACIL\n");
+
+     //AÑADIR FICHERO
+
+       scanf("%d",&dificultad);
+       system("cls");
+
+    switch(dificultad)
+    {
+    case 1:
+        printf("Has elegido la dificultad facil. \n");
+        printf("Comenzemos el juego.\n");
+        system("cls");
+        system("color 0A");//CAMBIAR EL COLOR DEL TEXTO
+        puntuacion= movimiento_pantalla(dificultad);
+    break;
+
+    case 2:
+        printf("Has elegido la dificultad medio. \n");
+        printf("Comenzemos el juego.\n");
+        system("cls");
+        system("color 0E");//CAMBIAR EL COLOR DEL TEXTO
+        puntuacion= movimiento_pantalla(dificultad);
+    break;
+
+    case 3:
+        printf("Has elegido la dificultad dificil. \n");
+        printf("Comenzemos el juego.\n");
+        system("cls");
+        system("color 0C");//CAMBIAR EL COLOR DEL TEXTO
+        puntuacion= movimiento_pantalla(dificultad);
+    break;
+
+    default:
+        printf("Opcion desconocida .\n");
+    break;
+
+    }
+    }while(dificultad!=1 && dificultad!=2 && dificultad!=3);
+
+
+
+         return 0;
+}
+
+
+
+int movimiento_pantalla(int dificultad) {
+
 punto xy;
 xy.x=28;
 xy.y=18;
@@ -21,7 +89,6 @@ punto punto_siguiente;
 punto_siguiente.x=28;
 punto_siguiente.y=18;
 int n=0, juego=0;
-int dificultad=1;
 char letra;
 
 //printf("El programa funciona con la interacción del usuario a partir de las teclas w (arriba), a (izquierda), x (abajo), d (derecha), y sus correspondientes mayúsculas./n Por ejemplo, si el usuario aprieta la tecla w el punto se desplaza haciaarriba una unidad,/n y si aprieta W se desplaza cinco unidades hacia arriba.\n");
@@ -75,18 +142,21 @@ system("cls");
 
 juego=pantalla(xy,dificultad);
 
-}while(letra != 'x');
+}while(xy.y!=0);
 
+printf("%c%cENHORABUENA!!%c%cHAS CONSEGUIDO SALIR!!", 173, 173, 173, 173);
 
 return 0;
 }
 
 
 int pantalla(punto xy, int dificultad){
+int puntuacion1, puntuacion2, puntuacion3, puntuacion_total;
+puntuacion_total= (puntuacion1 + puntuacion2 + puntuacion3)/3;
 int fil, col, caso=0;
-char pared=254, columna=219, columna_arriba=220, columna_abajo=223, pista=63, prueba=178;
+char pared=254, columna=219, columna_arriba=220, columna_abajo=223, pista=63;
 int pantalla[20][30]={
-	{05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05},
+	{05,13,13,13,13,13,13,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05},
 	{04,00,03,03,03,03,00,04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
 	{04,00,03,03,03,03,00,04,00,00,00,00,00,00,00,00,00,00,00,10,00,00,00,00,00,00,00,00,00,04},
 	{04,00,03,03,03,03,00,04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
@@ -100,7 +170,7 @@ int pantalla[20][30]={
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
-	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,05,00,02,05,05,05,04},
+	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,05,00,11,05,05,05,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04,00,00,00,00,00,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04,00,00,00,00,00,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04,00,00,00,00,00,04},
@@ -146,7 +216,7 @@ int pantalla[20][30]={
 
             }
 		    if(pantalla[fil][col]==2){
-                    printf("-");
+                    printf("%C", 196);
 
 		    }
 
@@ -155,7 +225,8 @@ int pantalla[20][30]={
 
 		    }
             if(pantalla[fil][col]==10){
-                    printf("%c", prueba);
+                    printf("%c", 245);
+
 
 		    }
 		    if(pantalla[fil][col]==3){
@@ -168,6 +239,11 @@ int pantalla[20][30]={
 		    }
 		     if(pantalla[fil][col]==5){
                     printf("%c", columna_arriba);
+
+		    }
+		    if(pantalla[fil][col]==13){
+                    printf("%c", columna_arriba);
+                    caso=4;
 
 		    }
 		     if(pantalla[fil][col]==6){
@@ -196,12 +272,12 @@ if(caso==3){
 
 
 if(caso==2){
+    //Blackjack(dificultad);
 
-   return 2;
 }
 
 
-	return 0;
+	return puntuacion_total;
 }
 
 
@@ -209,7 +285,7 @@ if(caso==2){
 void horizontal(punto *xy, punto *punto_siguiente, int n){
 
 int pantalla2[20][30]={
-	{05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05},
+	{05,13,13,13,13,13,13,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05},
 	{04,00,03,03,03,03,00,04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
 	{04,00,03,03,03,03,00,04,00,00,00,00,00,00,00,00,00,00,00,10,00,00,00,00,00,00,00,00,00,04},
 	{04,00,03,03,03,03,00,04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
@@ -223,7 +299,7 @@ int pantalla2[20][30]={
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
-	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,05,00,02,05,05,05,04},
+	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,05,00,11,05,05,05,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04,00,00,00,00,00,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04,00,00,00,00,00,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04,00,00,00,00,00,04},
@@ -234,7 +310,7 @@ int pantalla2[20][30]={
 punto_siguiente->x = punto_siguiente->x + n;
 
 
-    if(pantalla2[xy->y][punto_siguiente->x]==0 || pantalla2[xy->y][punto_siguiente->x]==8 || pantalla2[xy->y][punto_siguiente->x]==3 || pantalla2[xy->y][punto_siguiente->x]==7 || pantalla2[xy->y][punto_siguiente->x]==12){
+    if(pantalla2[xy->y][punto_siguiente->x]==0 || pantalla2[xy->y][punto_siguiente->x]==8 || pantalla2[xy->y][punto_siguiente->x]==3 || pantalla2[xy->y][punto_siguiente->x]==7 || pantalla2[xy->y][punto_siguiente->x]==12 || pantalla2[xy->y][punto_siguiente->x]==13){
         xy->x = punto_siguiente->x;
 
 }
@@ -248,7 +324,7 @@ punto_siguiente->x = punto_siguiente->x + n;
 void vertical(punto *xy, punto *punto_siguiente, int n){
 
 int pantalla2[20][30]={
-	{05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05},
+	{05,13,13,13,13,13,13,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05,05},
 	{04,00,03,03,03,03,00,04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
 	{04,00,03,03,03,03,00,04,00,00,00,00,00,00,00,00,00,00,00,10,00,00,00,00,00,00,00,00,00,04},
 	{04,00,03,03,03,03,00,04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
@@ -262,7 +338,7 @@ int pantalla2[20][30]={
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04},
-	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,05,00,02,05,05,05,04},
+	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,05,00,11,05,05,05,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04,00,00,00,00,00,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04,00,00,00,00,00,04},
 	{04,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,04,00,00,00,00,00,04},
@@ -273,7 +349,7 @@ int pantalla2[20][30]={
 
    punto_siguiente->y = punto_siguiente->y + n;
 
-    if(pantalla2[punto_siguiente->y][xy->x]==0 || pantalla2[punto_siguiente->y][xy->x]==8 || pantalla2[punto_siguiente->y][xy->x]==3 || pantalla2[punto_siguiente->y][xy->x]==7 || pantalla2[punto_siguiente->y][xy->x]==12){
+    if(pantalla2[punto_siguiente->y][xy->x]==0 || pantalla2[punto_siguiente->y][xy->x]==8 || pantalla2[punto_siguiente->y][xy->x]==3 || pantalla2[punto_siguiente->y][xy->x]==7 || pantalla2[punto_siguiente->y][xy->x]==12 || pantalla2[punto_siguiente->y][xy->x]==13){
         xy->y = punto_siguiente->y;
     }
     else{
